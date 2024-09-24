edcc604650b47b04e3657244165894c7694ed17a
misbahs100 <misbahuddins100@gmail.com>
2024-09-24 09:47:54 +0600

    modified hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -205,12 +205,6 @@ const Hero = () => {
                </div>
             </main>
 
-            <footer className="bg-gray-200">
-               <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-                  <a href="#" className="text-xl font-bold text-gray-500 hover:text-gray-400">MADE <sub>in</sub>CTG</a>
-                  <p className="py-2 text-gray-500 sm:py-0">All rights reserved</p>
-               </div>
-            </footer>
          </div>
 
 
